3ec96fc211c50b14b81b41407292c9bcb49cbd34
Mattia Aracne <mattia@codeatlas.it>
2023-01-10 18:10:02 +0100

    Minor fixes to the React plugin sources

--- src/events.tsx
+++ src/events.tsx
@@ -2,13 +2,14 @@ import {
   EmitterSubscription,
   NativeEventEmitter,
   NativeModules,
-} from 'react-native'
+} from 'react-native';
 
-export const CATAPUSH_MESSAGE_RECEIVED = 'Catapush#catapushMessageReceived'
-export const CATAPUSH_MESSAGE_SENT = 'Catapush#catapushMessageSent'
-export const CATAPUSH_NOTIFICATION_TAPPED = 'Catapush#catapushNotificationTapped'
-export const CATAPUSH_STATE_CHANGED = 'Catapush#catapushStateChanged'
-export const CATAPUSH_HANDLE_ERROR = 'Catapush#catapushHandleError'
+export const CATAPUSH_MESSAGE_RECEIVED = 'Catapush#catapushMessageReceived';
+export const CATAPUSH_MESSAGE_SENT = 'Catapush#catapushMessageSent';
+export const CATAPUSH_NOTIFICATION_TAPPED =
+  'Catapush#catapushNotificationTapped';
+export const CATAPUSH_STATE_CHANGED = 'Catapush#catapushStateChanged';
+export const CATAPUSH_HANDLE_ERROR = 'Catapush#catapushHandleError';
 
 const eventList = [
   CATAPUSH_MESSAGE_RECEIVED,
@@ -16,38 +17,38 @@ const eventList = [
   CATAPUSH_NOTIFICATION_TAPPED,
   CATAPUSH_STATE_CHANGED,
   CATAPUSH_HANDLE_ERROR,
-]
+];
 
 export default class EventManager {
-  catapushEventEmitter: NativeEventEmitter
-  notificationCache: Map<any, any>
-  eventHandlerMap: Map<any, any>
-  eventHandlerArrayMap: Map<any, any>
-  listeners: Map<string, EmitterSubscription>
+  catapushEventEmitter: NativeEventEmitter;
+  notificationCache: Map<any, any>;
+  eventHandlerMap: Map<any, any>;
+  eventHandlerArrayMap: Map<any, any>;
+  listeners: Map<string, EmitterSubscription>;
 
   constructor() {
-    this.notificationCache = new Map()
+    this.notificationCache = new Map();
     this.catapushEventEmitter = new NativeEventEmitter(
-      NativeModules.CatapushPluginModule
-    )
-    this.eventHandlerMap = new Map()
-    this.eventHandlerArrayMap = new Map()
-    this.listeners = new Map()
-    this.setupListeners()
+      NativeModules.CatapushPluginModule,
+    );
+    this.eventHandlerMap = new Map();
+    this.eventHandlerArrayMap = new Map();
+    this.listeners = new Map();
+    this.setupListeners();
   }
 
   setupListeners() {
     // set up the event emitter and listeners
     for (let i = 0; i < eventList.length; i++) {
-      let eventName = eventList[i]
-      this.listeners.set(eventName, this.generateEventListener(eventName))
+      let eventName = eventList[i];
+      this.listeners.set(eventName, this.generateEventListener(eventName));
     }
   }
 
   // clear handlers
   clearHandlers() {
-    this.eventHandlerMap = new Map()
-    this.eventHandlerArrayMap = new Map()
+    this.eventHandlerMap = new Map();
+    this.eventHandlerArrayMap = new Map();
   }
 
   /**
@@ -57,28 +58,30 @@ export default class EventManager {
    * @param  {function} handler
    */
   setEventHandler(eventName: string, handler: any) {
-    this.eventHandlerMap.set(eventName, handler)
+    this.eventHandlerMap.set(eventName, handler);
   }
 
   /**
    * clears the event handler(s) for the event name
    * @param  {string} eventName
-   * @param  {function} handler
    */
   clearEventHandler(eventName: string) {
-    this.eventHandlerArrayMap.delete(eventName)
+    this.eventHandlerArrayMap.delete(eventName);
   }
 
   // returns an event listener with the js to native mapping
   generateEventListener(eventName: string) {
     const addListenerCallback = (payload: any) => {
-      let handler = this.eventHandlerMap.get(eventName)
+      let handler = this.eventHandlerMap.get(eventName);
       // Check if we have added listener for this type yet
       if (handler) {
-        handler(payload)
+        handler(payload);
       }
-    }
+    };
 
-    return this.catapushEventEmitter.addListener(eventName, addListenerCallback)
+    return this.catapushEventEmitter.addListener(
+      eventName,
+      addListenerCallback,
+    );
   }
 }
